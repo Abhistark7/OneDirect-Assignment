60ae2fe7ece1d698aebfa537c6fd3656c723228b
Abhishek Sahu <abhistark7@gmail.com>
2019-01-06 05:40:23 +0530

    Add files via upload

---
+++ assignmentQuestion5/src/main/java/com/OD/Question5/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.OD.Question5.repository;
+
+import com.OD.Question5.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
